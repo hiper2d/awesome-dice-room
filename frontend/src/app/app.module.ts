306f557348d95b1e42f1c29b87c5b569a398afd6
Alexey Zelenovsky <hiper2d@gmail.com>
2019-01-02 19:01:49 -0500

    Added tabs

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material';
+import {MaterialModule} from './shared/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import {MatButtonModule} from '@angular/material';
   ],
   imports: [
     BrowserAnimationsModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/shared/material.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {MatButtonModule, MatTabsModule} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatTabsModule
+  ],
+  declarations: [
+  ],
+  exports: [
+    MatButtonModule,
+    MatTabsModule
+  ]
+})
+export class MaterialModule {
+}
