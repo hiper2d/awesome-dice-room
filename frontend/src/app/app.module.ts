c4f0798be35edd4a281343308afb4d228704a07d
Alexey Zelenovsky <hiper2d@gmail.com>
2019-01-03 19:50:46 -0500

    Returned BrowserModule since it exists in docs examples

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import {RoomModule} from './room/room.module';
 import {DashboardModule} from './dashboard/dashboard.module';
 import {CoreModule} from './core/core.module';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
     PageNotFoundComponent,
   ],
   imports: [
+    BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
     DashboardModule,
