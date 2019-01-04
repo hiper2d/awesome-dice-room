9563d2639cd2ecaec58bd1e5f468e53eeaa233f4
Alexey Zelenovsky <hiper2d@gmail.com>
2019-01-03 19:48:16 -0500

    Removed BrowserModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {RoomModule} from './room/room.module';
 import {DashboardModule} from './dashboard/dashboard.module';
 import {CoreModule} from './core/core.module';
-import {BrowserModule} from '@angular/platform-browser';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 @NgModule({
@@ -15,7 +13,6 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
     PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
     DashboardModule,
