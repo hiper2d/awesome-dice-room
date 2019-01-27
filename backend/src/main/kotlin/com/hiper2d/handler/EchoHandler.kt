8b93b5f73a1922fd7046d5d6624739406d5bbd96
Alexey Zelenovsky <hiper2d@gmail.com>
2019-01-27 16:56:40 -0500

    Prepared backend api for room (crud operations)

--- backend/src/main/kotlin/com/hiper2d/handler/EchoHandler.kt
+++ backend/src/main/kotlin/com/hiper2d/handler/EchoHandler.kt
@@ -5,16 +5,18 @@ import com.hiper2d.repository.EchoRepository
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.bodyToMono
+import reactor.core.publisher.Mono
 
 @Component
 class EchoHandler(private val echoRepository: EchoRepository) {
 
-    fun saveX(req: ServerRequest) =  ok().contentType(MediaType.APPLICATION_JSON_UTF8)
+    fun saveX(req: ServerRequest): Mono<ServerResponse> = ok().contentType(MediaType.APPLICATION_JSON_UTF8)
         .body(req.bodyToMono<Echo>().flatMap { echoRepository.save(it) }, Echo::class.java)
 
-    fun readAll(req: ServerRequest) = ok()
+    fun readAll(req: ServerRequest): Mono<ServerResponse> = ok()
         .contentType(MediaType.APPLICATION_JSON_UTF8)
         .body(echoRepository.findAll(), Echo::class.java)
 }
--- backend/src/main/kotlin/com/hiper2d/router/EchoRouter.kt
+++ backend/src/main/kotlin/com/hiper2d/router/EchoRouter.kt
@@ -4,18 +4,20 @@ import com.hiper2d.handler.EchoHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 
 @Configuration
-class TestRouter {
+class EchoRouter {
 
     @Bean
-    fun router(EchoHandler: EchoHandler) = router {
+    fun echoRoute(echoHandler: EchoHandler): RouterFunction<ServerResponse> = router {
         GET("/api/tests").nest {
-            accept(MediaType.TEXT_HTML, EchoHandler::readAll)
+            accept(MediaType.TEXT_HTML, echoHandler::readAll)
         }
         POST("/api/test").nest {
-            accept(MediaType.APPLICATION_JSON_UTF8, EchoHandler::saveX)
+            accept(MediaType.APPLICATION_JSON_UTF8, echoHandler::saveX)
         }
     }
 }
