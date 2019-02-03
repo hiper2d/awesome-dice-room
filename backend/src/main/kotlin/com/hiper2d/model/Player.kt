18b452e25793806863aee8fef0f9183c6d74a4d5
Alexey Zelenovsky <hiper2d@gmail.com>
2019-02-02 19:31:15 -0500

    Added create and read operations for players on backend Added webflux integration test

---
+++ backend/src/main/kotlin/com/hiper2d/model/Player.kt
@@ -0,0 +1,6 @@
+package com.hiper2d.model
+
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "player")
+data class Player(val id: String? = null, val userId: String, val name: String)
