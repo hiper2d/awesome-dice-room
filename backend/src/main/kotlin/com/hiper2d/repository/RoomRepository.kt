a70bd6400d656cfd7b58e7738b61ab3946353ba9
ventolingsv <ventolingsv@gmail.ru>
2019-02-19 00:02:48 +0100

    fixes

--- backend/src/main/kotlin/com/hiper2d/repository/RoomRepository.kt
+++ backend/src/main/kotlin/com/hiper2d/repository/RoomRepository.kt
@@ -2,6 +2,5 @@ package com.hiper2d.repository
 
 import com.hiper2d.model.Room
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import org.springframework.stereotype.Repository
 
 interface RoomRepository: ReactiveMongoRepository<Room, String>, RoomRepositoryCustom
---
+++ backend/src/main/kotlin/com/hiper2d/util/ReactiveUtils.kt
@@ -0,0 +1,20 @@
+package com.hiper2d.util
+
+import reactor.core.publisher.Mono
+
+fun <T> Mono<T>.isEmpty(): Boolean = this == Mono.empty<T>()
+
+fun <T, R> Mono<T?>.mapIfNotEmpty(f: (T) -> R): Mono<R> {
+    return if (this.isEmpty()) Mono.empty()
+    else this.map { v -> f(v!!) }
+}
+
+fun <T, R> Mono<T?>.flatMapIfNotEmpty(f: (T) -> Mono<R>): Mono<R> {
+    return if (this.isEmpty()) Mono.empty()
+    else this.flatMap { v -> f(v!!) }
+}
+
+fun <T> Mono<T?>.filterWithEmpty(p: (T) -> Boolean): Mono<T?> {
+    return if (this.isEmpty()) return this
+    else this.filter { v -> p(v!!) }
+}
