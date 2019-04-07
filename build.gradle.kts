8101ec9aaa04a2137b98ffa1194c33845259793b
Alexey Zelenovsky <hiper2d@gmail.com>
2019-04-06 20:12:58 -0400

    Created global Gradle config and made the backend a subproject

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("com.bmuschko.docker-remote-api") version "4.6.2" apply false
+}
+
+subprojects {
+    group = "com.hiper2d"
+    version = "1.0"
+
+    apply {
+        plugin("org.gradle.base")
+        plugin("com.bmuschko.docker-remote-api")
+    }
+
+    tasks["build"].finalizedBy("buildDockerImage")
+}
